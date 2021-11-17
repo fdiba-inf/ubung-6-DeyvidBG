c7cddd87d6a62e3dbae68d6167604ffef9601c4d
Deyvid <>
2021-11-17 21:35:41 +0000

    02.00

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,26 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String password = input.next();
+
+        if (password.length() < 8) {   
+          // its not valid
+        } else {      
+          char c;  
+          int count = 0;   
+          for (int i = 0; i < password.length(); i++) {  
+            c = password.charAt(i);  
+            if (!Character.isLetterOrDigit(c)) {          
+                // its not a valid password
+            } else if (Character.isDigit(c)) {  
+                count++;     
+            }  
+          }
+          if (count < 2)   {     
+            // its not valid
+          } else {
+            System.out.println("Password valid!");  
+          }  
+        }
     }
 }
